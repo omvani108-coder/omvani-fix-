819d019941825d22e9b1c1e92fb5eae3c0392c52
omvani108-coder <omvani108@gmail.com>
2026-02-18 21:31:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Scriptures from "@/pages/scriptures/Scriptures";
+import { BottomNav } from "@/components/BottomNav";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/contexts/AuthContext";
@@ -50,6 +51,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </AuthProvider>
+         <BottomNav />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
